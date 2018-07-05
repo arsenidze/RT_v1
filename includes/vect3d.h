cf590ac2b47a39c97e42497b5c0468a12fbd3802
Arseniy MELIHOV <amelihov@e1r1p1.unit.ua>
2018-07-05 20:46:33 +0300

    change macros on functions for vect3d

--- includes/vect3d.h
+++ includes/vect3d.h
@@ -6,7 +6,7 @@
 /*   By: amelihov <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/09 18:11:34 by amelihov          #+#    #+#             */
-/*   Updated: 2018/07/05 20:13:19 by amelihov         ###   ########.fr       */
+/*   Updated: 2018/07/05 20:27:45 by amelihov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,19 +21,19 @@ typedef double	t_vect3d __attribute__((vector_size(sizeof(double)*3)));
 # define Y 1
 # define Z 2
 
-# define VECT3D(x, y, z) (t_vect3d){(x), (y), (z)}
-# define VECT3D_MULT_ON_SCALAR(v, a) ((v) * VECT3D(a, a, a))
-# define VECT3D_DIV_ON_SCALAR(v, a) ((v) / VECT3D(a, a, a))
-# define VECT3D_LEN(v) (sqrt(VECT3D_SQ_LEN(v)))
-# define VECT3D_NORM(v) VECT3D_DIV_ON_SCALAR((v), VECT3D_LEN(v))
-# define VECT3D_SQ_LEN(v) ((v)[X] * (v)[X] + (v)[Y] * (v)[Y] + (v)[Z] * (v)[Z])
-# define VECT3D_DOT(v1, v2) (v1[X] * v2[X] + v1[Y] * v2[Y] + v1[Z] * v2[Z])
-# define VECT3D_CROSS(v1, v2) VECT3D(v1[Y] * v2[Z] - v1[Z] * v2[Y], \
-									v1[Z] * v2[X] - v1[X] * v2[Z], \
-									v1[X] * v2[Y] - v1[Y] * v2[X])
-# define VECT3D_COS(v1, v2) (VECT3D_DOT(v1, v2) / (VECT3D_LEN(v1) * VECT3D_LEN(v2)))
-# define VECT3D_COS_NORMED(v1, v2) VECT3D_DOT(v1, v2)
-# define VECT3D_APPLY(v,f,...) VECT3D(f(v[X],__VA_ARGS__),f(v[Y],__VA_ARGS__),f(v[Z],__VA_ARGS__))
+//# define VECT3D(x, y, z) (t_vect3d){(x), (y), (z)}
+//# define VECT3D_MULT_ON_SCALAR(v, a) ((v) * VECT3D(a, a, a))
+//# define VECT3D_DIV_ON_SCALAR(v, a) ((v) / VECT3D(a, a, a))
+//# define VECT3D_LEN(v) (sqrt(VECT3D_SQ_LEN(v)))
+//# define VECT3D_NORM(v) VECT3D_DIV_ON_SCALAR((v), VECT3D_LEN(v))
+//# define VECT3D_SQ_LEN(v) ((v)[X] * (v)[X] + (v)[Y] * (v)[Y] + (v)[Z] * (v)[Z])
+//# define VECT3D_DOT(v1, v2) (v1[X] * v2[X] + v1[Y] * v2[Y] + v1[Z] * v2[Z])
+//# define VECT3D_CROSS(v1, v2) VECT3D(v1[Y] * v2[Z] - v1[Z] * v2[Y], \
+//									v1[Z] * v2[X] - v1[X] * v2[Z], \
+//									v1[X] * v2[Y] - v1[Y] * v2[X])
+//# define VECT3D_COS(v1, v2) (VECT3D_DOT(v1, v2) / (VECT3D_LEN(v1) * VECT3D_LEN(v2)))
+//# define VECT3D_COS_NORMED(v1, v2) VECT3D_DOT(v1, v2)
+//# define VECT3D_APPLY(v,f,...) VECT3D(f(v[X],__VA_ARGS__),f(v[Y],__VA_ARGS__),f(v[Z],__VA_ARGS__))
 //t_vect3d		vect3d_cross(t_vect3d v1, t_vect3d v2);
 short		vect3d_is_in_range(t_vect3d v, double a, double b);
 short		vect3d_is_in_range_abs(t_vect3d v, double a, double b);
@@ -42,17 +42,16 @@ void		vect3d_fill_rotateOX_matrix(t_vect3d m[3], double angel);
 void		vect3d_fill_rotateOY_matrix(t_vect3d m[3], double angel);
 void		vect3d_fill_rotateOZ_matrix(t_vect3d m[3], double angel);
 
-//t_vect3d	vect3d(double x, double y, double z); (t_vect3d){(x), (y), (z)}
-//t_vect3d	vect3d_mult_on_scalar(v, a) ((v) * VECT3D(a, a, a))
-//t_vect3d	vect3d_div_on_scalar(v, a) ((v) / VECT3D(a, a, a))
-//double		vect3d_len(v) (sqrt(VECT3D_SQ_LEN(v)))
-//t_vect3d	vect3d_norm(v) VECT3D_DIV_ON_SCALAR((v), VECT3D_LEN(v))
-//double		vect3d_sq_len(v) ((v)[X] * (v)[X] + (v)[Y] * (v)[Y] + (v)[Z] * (v)[Z])
-//t_vect3d	vect3d_dot(v1, v2) (v1[X] * v2[X] + v1[Y] * v2[Y] + v1[Z] * v2[Z])
-//t_vect3d	vect3d_cross(v1, v2) VECT3D(v1[Y] * v2[Z] - v1[Z] * v2[Y], \
-//									v1[Z] * v2[X] - v1[X] * v2[Z], \
-//									v1[X] * v2[Y] - v1[Y] * v2[X])
-# define VECT3D_COS(v1, v2) (VECT3D_DOT(v1, v2) / (VECT3D_LEN(v1) * VECT3D_LEN(v2)))
-# define VECT3D_COS_NORMED(v1, v2) VECT3D_DOT(v1, v2)
+t_vect3d	vect3d(double x, double y, double z);
+t_vect3d	vect3d_mult_on_scalar(t_vect3d v, double a);
+t_vect3d	vect3d_div_on_scalar(t_vect3d v, double a);
+double		vect3d_len(t_vect3d v);
+t_vect3d	vect3d_norm(t_vect3d v);
+double		vect3d_sq_len(t_vect3d v);
+t_vect3d	vect3d_dot(t_vect3d v1, t_vect3d v2);
+t_vect3d	vect3d_cross(t_vect3d v1, t_vect3d v2);
+double		vect3d_cos(t_vect3d v1, t_vect3d v2);
+double		vect3d_cos_normed(t_vect3d v1, t_vect3d v2);
+t_vect3d	vect3d_clamp(t_vect3d v, double a, double b);
 
 #endif
